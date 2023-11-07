68429fa0f6e3408b24cfa8c7b0746a35d550eabf
enderNakamoto <ender.nakamoto@gmail.com>
2023-11-06 21:38:01 -0800

    added fake data for tesla api vehicle data generation

--- src/app/api/serverless-example/route.ts
+++ src/app/api/serverless-example/route.ts
@@ -1,15 +1,22 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-export function GET(request: NextRequest) {
-  return NextResponse.json(
-    {
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { vehicle_data } from '@/lib/vehicle_data';
+
+type ResponseData = {
+  message: string
 }
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const {query: { high_mileage, fast_driver }, method } = req;
+
+  console.log("high_mileage", high_mileage); 
+  console.log("fast_driver", fast_driver);
+  
+  res.status(200).json({ message: 'Hello from Next.js!' })
+}
+
+
+ 
--- src/lib/vehicle_data.ts
+++ src/lib/vehicle_data.ts
@@ -285,3 +285,20 @@
 //      "webcam_available": true
 //     }
 //    }
+
+import { faker } from '@faker-js/faker';
+
+import { vehicle_state } from "./vehicle_state";
+import { drive_state } from "./drive_state";
+
+export function vehicle_data(high_mileage: boolean, fast_driver: boolean) {
+
+    const result = {
+        "id": faker.number.int(256), 
+        "vin": faker.vehicle.vin(),
+        "color": faker.color.human(),
+        "vehicle_state": vehicle_state(high_mileage),
+        "drive_state": drive_state(fast_driver)
+   }
+    return result; 
+}
